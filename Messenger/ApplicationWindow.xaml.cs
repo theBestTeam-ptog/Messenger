777168b63ca8cb327d2b06d9169fe4f13d0e8740
alexzonic <smailik072001@mail.ru>
2021-03-20 14:33:47 +0400

    разделены окна для аторизации и самого приложения

---
+++ Messenger/ApplicationWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace Messenger
+{
+    public partial class ApplicationWindow : Window
+    {
+        public ApplicationWindow()
+        {
+            InitializeComponent();
+            
+            frame.NavigationService.Navigate(new Uri("Pages/PageNoDialog.xaml", UriKind.Relative));
+        }
+        
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            frame.Source = null;
+            frame.Navigate(new Uri("Pages/Dialog.xaml", UriKind.Relative));
+        }
+    }
+}
--- Messenger/MainWindow.xaml.cs
+++ Messenger/MainWindow.xaml.cs
@@ -11,14 +11,8 @@ namespace Messenger
         public MainWindow()
         {
             InitializeComponent();
-            
-            frame.NavigationService.Navigate(new Uri("Pages/PageNoDialog.xaml", UriKind.Relative));
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            frame.Source = null;
-            frame.Navigate(new Uri("Pages/Dialog.xaml", UriKind.Relative));
-        }
+        
     }
 }
