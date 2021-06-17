248231c8400da42962eba60cb200e5c38cc81a2a
kruglov <kruglov@o-code.ru>
2021-06-17 17:04:41 +0400

    ыва

---
+++ Messenger/ViewModels/ChatViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using Domain.Models;
+
+namespace Messenger.ViewModels
+{
+    public sealed class ChatViewModel
+    {
+        public string ChatName { get; set; }
+        public ObservableCollection<Message> History { get; set; }
+        public string ChatId { get; set; }
+        
+        public UserInfo[] UserInfos { get; set; }
+    }
+
+    public sealed class UserInfo
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ Messenger/ViewModels/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using JetBrains.Annotations;
+
+namespace Messenger.ViewModels
+{
+    [UsedImplicitly]
+    public sealed class UserViewModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public Image ProfileImage { get; set; }
+        public List<ChatViewModel> Chats { get; set; }
+        public bool InNetwork { get; set; }
+    }
+}
